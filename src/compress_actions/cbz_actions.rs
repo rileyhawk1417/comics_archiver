91c9cd4a7e4db550c2594050d21437b77e2e9f0a
Sean Riley Hawkins <rileyhawk249@gmail.com>
2024-03-04 19:35:06 +0200

    chore: remove unused implementations

--- src/compress_actions/cbz_actions.rs
+++ src/compress_actions/cbz_actions.rs
@@ -6,42 +6,6 @@ use tokio::io::{AsyncBufRead, AsyncRead, AsyncReadExt, AsyncSeekExt, BufReader a
 use tokio::runtime::Runtime;
 use zip::{write::FileOptions, CompressionMethod, ZipArchive, ZipWriter};
 
-struct AsyncBufReaderAdapter(tokio::fs::File);
-
-/// Adapter to satisfy BufReader from tokio
-/// Since ZipArchive doesn't implement AsyncReadExt
-impl Read for AsyncBufReaderAdapter {
-    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
-        let rt = Runtime::new().unwrap();
-        let res: io::Result<usize> = rt.block_on(async move {
-            let mut buf_read = AsyncBufReader::new(&mut self.0);
-            let mut data = vec![0; buf.len()];
-            let bytes_read: usize = buf_read.read(&mut data).await?;
-            buf[..bytes_read].copy_from_slice(&data[..bytes_read]);
-            Ok(bytes_read)
-        });
-        match res {
-            Ok(bytes_read) => Ok(bytes_read),
-            Err(err) => Err(Into::into(err)),
-        }
-    }
-}
-//BUG: Seek is struggling with rt block on
-/// Adapter to satisfy BufReader from tokio
-/// Since ZipArchive doesn't implement AsyncSeekExt
-impl Seek for AsyncBufReaderAdapter {
-    fn seek(&mut self, pos: SeekFrom) -> io::Result<u64> {
-        let rt = Runtime::new().unwrap();
-        let result = rt.block_on(async {
-            return self.0.seek(pos).await;
-        });
-        match result {
-            Ok(bytes_read) => Ok(bytes_read),
-            Err(err) => Err(Into::into(err)),
-        }
-    }
-}
-
 /// Extract files from `.cbz` archive.
 /// Then return archive entries.
 /// * `cbz_file`: `.cbz file`
